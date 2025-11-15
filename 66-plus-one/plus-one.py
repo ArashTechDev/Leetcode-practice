080ff105a336dc2f535d6611ca46bdeb64528227
Ankit Thapar <knowergenius1922@gmail.com>
2025-11-15 00:30:53 -0500

    modified

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -8,7 +8,7 @@ class Solution:
             newArr = digits
             return newArr
         else:
-            digits[maxIndex] = 0
+            digits[maxIndex] = 
             carry = 1
 
             while carry == 1 and maxIndex > 0:
